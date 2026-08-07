a400db29a8856ed66a8fa297e69cc947ab3551cd
Ajay Agwan <agwanajay28@gmail.com>
2026-08-07 03:30:00 -0400

    StringPool

---
+++ String/src/StringLiterals_SCP/DemoString.java
@@ -0,0 +1,33 @@
+package StringLiterals_SCP;
+
+public class DemoString {
+	public static void main(String[] args) {
+
+//		String name = "Ajay";
+//		String s = new String("Ajay");
+//		System.out.println(name);
+//		System.out.println(s);
+//
+//		System.out.println(System.identityHashCode(name));
+//		System.out.println(System.identityHashCode(s));
+
+//		String name = "Swapnil";
+//		String fname = "Swapnil + Supekar";// In case String literals assign new memory location we cannot update it. 
+
+//		System.out.println(name);
+//		System.out.println(fname);
+//		
+//		System.out.println(name==fname);
+
+//		System.out.println(System.identityHashCode(name));//
+//		System.out.println(System.identityHashCode(fname));
+		
+		String s1 = new String("Hello");
+		String s2 = new String("Hello");
+
+		System.out.println(s1 == s2);        // false
+		System.out.println(s1.equals(s2));   // true
+		
+
+	}
+}
