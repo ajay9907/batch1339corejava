2784aab30b78d2f3d70668708ec13b48cad1cf4a
Ajay Agwan <agwanajay28@gmail.com>
2026-08-07 04:56:47 -0400

    new

--- String/src/StringLiterals_SCP/DemoString.java
+++ String/src/StringLiterals_SCP/DemoString.java
@@ -21,13 +21,22 @@ public class DemoString {
 
 //		System.out.println(System.identityHashCode(name));//
 //		System.out.println(System.identityHashCode(fname));
-		
-		String s1 = new String("Hello");
-		String s2 = new String("Hello");
 
-		System.out.println(s1 == s2);        // false
-		System.out.println(s1.equals(s2));   // true
-		
+		String s1 = ("Hello");
+		String s = s1.concat("Ajay");
+		String s2 = s1 + s;
+
+		System.out.println(s1.equals(s2));
+		System.out.println(s1 == s2);
+//		System.out.println(s1.equals(s));
+//
+//		System.out.println(s1.hashCode());
+//		System.out.println(s.hashCode());
+//		System.out.println(s2.hashCode());
+
+//		System.out.println(System.identityHashCode(s1));
+//		System.out.println(System.identityHashCode(s));
+//		System.out.println(System.identityHashCode(s2));
 
 	}
 }
