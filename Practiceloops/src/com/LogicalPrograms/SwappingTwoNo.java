8609dc72d6b30662f83b26c9f9c515ccd3d2e3ec
Ajay Agwan <agwanajay28@gmail.com>
2026-08-08 12:42:38 -0400

    .

--- Practiceloops/src/com/LogicalPrograms/SwappingTwoNo.java
+++ Practiceloops/src/com/LogicalPrograms/SwappingTwoNo.java
@@ -4,15 +4,14 @@ package com.LogicalPrograms;
 public class SwappingTwoNo {
 
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 20;
+		int a = 12;
+		int b = 14;
 
 		a = a + b;
 		b = a - b;
 		a = a - b;
-		System.out.println("a=" + a);
-		System.out.println("b=" + b);
-
+		System.out.println("a =" + a);
+		System.out.println("b =" + b);
 	}
 
 }
