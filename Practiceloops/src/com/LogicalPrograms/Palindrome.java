9888b0791a535fa4e7e1bccb8a9c74af6ec239d2
Ajay Agwan <agwanajay28@gmail.com>
2026-08-13 05:56:06 -0400

    .

---
+++ Practiceloops/src/com/LogicalPrograms/Palindrome.java
@@ -0,0 +1,21 @@
+package com.LogicalPrograms;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		int num = 121;
+		int original = num;
+		int rev = 0;
+		while (num > 0) {
+			int digit = num % 10;
+			rev = rev * 10 + digit;
+			num = num / 10;
+
+		}
+		if (original == rev) {
+			System.out.println("Is palindrome number.");
+
+		} else {
+			System.out.println("Is not Palindrome");
+		}
+	}
+}
