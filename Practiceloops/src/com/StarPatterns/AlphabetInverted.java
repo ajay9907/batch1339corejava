2db6c1be463b7c85a075606907756eec86dc441d
Ajay Agwan <agwanajay28@gmail.com>
2026-08-13 04:44:27 -0400

    .

---
+++ Practiceloops/src/com/StarPatterns/AlphabetInverted.java
@@ -0,0 +1,14 @@
+package com.StarPatterns;
+
+public class AlphabetInverted {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int row = 1; row <= n; row++) {
+			// for each row--> variable columns.
+			for (int col = 1; col <= n - row + 1; col++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
