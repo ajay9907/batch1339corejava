78bc739df569606c7f7c929d7502f1cd987cbb1a
Ajay Agwan <agwanajay28@gmail.com>
2026-08-14 01:26:26 -0400

    .

--- Practiceloops/src/com/LogicalPrograms/Palindrome.java
+++ Practiceloops/src/com/LogicalPrograms/Palindrome.java
@@ -1,8 +1,15 @@
 package com.LogicalPrograms;
 
+import java.util.Scanner;
+
 public class Palindrome {
 	public static void main(String[] args) {
-		int num = 121;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter Number :");
+
+		int num = sc.nextInt();
+		
 		int original = num;
 		int rev = 0;
 		while (num > 0) {
---
+++ Practiceloops/src/com/LogicalPrograms/PrimaryNo.java
@@ -0,0 +1,27 @@
+package com.LogicalPrograms;
+
+import java.util.Scanner;
+
+public class PrimaryNo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter Number :");
+		int num = sc.nextInt();
+
+		int count = 0;
+
+		for(int i=2;i<=num/2;i++) {
+			if(num%i==0) {
+				count++;
+			}
+		}
+			if (count == 0) {
+				System.out.println("Is Prime no");
+			} else {
+				System.out.println("Is not Prime.");
+			}
+		}
+
+	}
+
