2c7ea0e2f1e86996af71e9976a744dc45fe08fad
Ajay Agwan <agwanajay28@gmail.com>
2026-08-18 04:46:52 -0400

    .

---
+++ String/src/String/String_Operation.java
@@ -0,0 +1,64 @@
+package String;
+
+public class String_Operation {
+	public static void main(String[] args) {
+
+//		String str = "Ajay";
+//		String str2 = new String("Agwan");
+
+//		System.out.println(str.charAt(2));
+//
+//		System.out.println(str.codePointAt(1));
+//
+//		System.out.println(str.codePointBefore(3));
+
+//		System.out.println(str.compareToIgnoreCase(str));
+
+//		System.out.println(str.concat(str2));
+
+//		System.out.println(str.contains(str2));
+
+//		System.out.println(str.hashCode());
+//		System.out.println(str2.hashCode());
+
+//		System.out.println(System.identityHashCode(str2));
+//		System.out.println(System.identityHashCode(str));
+
+		String str1 = "Ajay";
+		String str3 = new String("Agwan");
+
+//		System.out.println(str.contentEquals(str2));
+//
+//		String str4 = "Ajay";
+//		String str5 = new String("Agwannn");
+//
+//		System.out.println(str.contentEquals("Ajyaa"));
+//
+//		System.out.println(str4.endsWith(str));
+
+		String name = "Ajay";
+		String fname = new String("AjayA");
+//		System.out.println(name.equals(fname));
+
+//		System.out.println(name.equalsIgnoreCase(str1));
+
+//		System.out.println(name.indent(1));
+
+//		System.out.println(name.indexOf(str1));
+
+//		System.out.println(fname.intern());
+
+//		System.out.println(name.isBlank());
+		
+//		System.out.println(name.isEmpty());
+		
+//		System.out.println(name.length());
+		
+//		System.out.println(name.repeat(3));
+		
+		System.out.println(name.replace("Ajay", "Ajyaa"));
+		
+
+	}
+
+}
--- String/src/String/String_Palindrome.java
+++ String/src/String/String_Palindrome.java
@@ -1,12 +1,17 @@
 package String;
 
+import java.util.Scanner;
+
 public class String_Palindrome {
 	public static void main(String[] args) {
-		String str = "madam";
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter String :");
+		String str = sc.nextLine();
+
 		String rev = new StringBuilder(str).reverse().toString();
 
 		if (str.equals(rev)) {
-			System.out.println("Is Palindrome");
+			System.out.println("Is Palindrome :" + str);
 
 		} else {
 			System.out.println("Is not Palindrome");
