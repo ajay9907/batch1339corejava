71bdf1538ca68599c1e375042899225b5d5d5d1f
Ajay Agwan <agwanajay28@gmail.com>
2026-08-19 14:06:05 -0400

    .

--- Array/src/com/Array/Sum.java
+++ Array/src/com/Array/Sum.java
@@ -1,18 +1,24 @@
 package com.Array;
 
+import java.util.Scanner;
+
 public class Sum {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter Numbers: ");
 
 //		int num[] = { 10 };
 //		int num1[] = { 20 };
 //		for (int i = 0; i < num.length; i++) {
 //			System.out.println("Addition Of Two Arrays:" + (num[i] + num1[i]));
 //		}
-		int num[] = { 10, 20, 30, 40, 50 };
+		int num[] = new int[5];
 
 		int sum = 0;
 
 		for (int i = 0; i < num.length; i++) {
+			num[i] = sc.nextInt();
 			sum = sum + num[i];
 		}
 		System.out.println("Addition of All Elements:" + sum);
