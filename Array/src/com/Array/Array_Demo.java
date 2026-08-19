bbcbf52332576a880cdddd34ba5b03ee8258a2c6
Ajay Agwan <agwanajay28@gmail.com>
2026-08-19 11:59:47 -0400

    .

---
+++ Array/src/com/Array/Array_Demo.java
@@ -0,0 +1,17 @@
+package com.Array;
+
+public class Array_Demo {
+	public static void main(String[] args) {
+
+		// declaration of Static array
+		int[] arr = { 1, 3, 2, 4, 6 };
+
+//		for (int i = 0; i < arr.length; i++) {
+//			System.out.println(arr[i]);
+//		}
+		for (int i : arr) {
+
+		}
+
+	}
+}
--- String/src/String/Duplicate_String.java
+++ String/src/String/Duplicate_String.java
@@ -2,22 +2,22 @@ package String;
 
 public class Duplicate_String {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        String str = "programming";
+		String str = "programming";
 
-        System.out.println("Duplicate characters:");
+		System.out.println("Duplicate characters:");
 
-        for (int i = 0; i < str.length(); i++) {
+		for (int i = 0; i < str.length(); i++) {
 
-            for (int j = i + 1; j < str.length(); j++) {
+			for (int j = i + 1; j < str.length(); j++) {
 
-                if (str.charAt(i) == str.charAt(j)) {
+				if (str.charAt(i) == str.charAt(j)) {
 
-                    System.out.println(str.charAt(i));
-                    break;
-                }
-            }
-        }
-    }
+					System.out.println(str.charAt(i));
+					break;
+				}
+			}
+		}
+	}
 }
