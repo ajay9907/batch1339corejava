90cc4bd29f09b61acd06fcfad81d6352e5c59a20
Ajay Agwan <agwanajay28@gmail.com>
2026-08-19 03:32:18 -0400

    .

--- Practiceloops/src/com/LogicalPrograms/Reverse.java
+++ Practiceloops/src/com/LogicalPrograms/Reverse.java
@@ -6,18 +6,13 @@ public class Reverse {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Enter Numbers: ");
-		int num = sc.nextInt();
-
+		int num = 12345;
 		int rev = 0;
 		while (num > 0) {
-			int digit = num % 10;
-			rev = rev * 10 + digit;
-			num /= 10;
+			rev = num % 10;
+			System.out.print(rev);
+			num = num / 10;
 
-			System.out.print("\nReverse Number is : " + rev);
 		}
 	}
 }
