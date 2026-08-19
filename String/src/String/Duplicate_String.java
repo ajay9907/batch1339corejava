6759fe0fee3ad6e842afc4e471f908eb1cf2ba68
Ajay Agwan <agwanajay28@gmail.com>
2026-08-19 06:32:57 -0400

    .

---
+++ String/src/String/Duplicate_String.java
@@ -0,0 +1,23 @@
+package String;
+
+public class Duplicate_String {
+
+    public static void main(String[] args) {
+
+        String str = "programming";
+
+        System.out.println("Duplicate characters:");
+
+        for (int i = 0; i < str.length(); i++) {
+
+            for (int j = i + 1; j < str.length(); j++) {
+
+                if (str.charAt(i) == str.charAt(j)) {
+
+                    System.out.println(str.charAt(i));
+                    break;
+                }
+            }
+        }
+    }
+}
