88e052537563bb8d7878b2f0727c2dd3f2865bb7
Ajay Agwan <agwanajay28@gmail.com>
2026-08-20 11:59:28 -0400

    .

---
+++ Array/src/com/Array/Max_No.java
@@ -0,0 +1,18 @@
+package com.Array;
+
+public class Max_No {
+	public static void main(String[] args) {
+
+		int arr[] = { 34, 432, 344, 35, 654, 345 };
+
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+
+			}
+		}
+		System.out.println("Max No:" + max);
+	}
+
+}
