f05d11ba03183c04a3c71b0b89eb42ed63ae6d8e
Ajay Agwan <agwanajay28@gmail.com>
2026-08-20 13:30:31 -0400

    /

---
+++ Array/src/com/Array/Reversed_Array.java
@@ -0,0 +1,13 @@
+package com.Array;
+
+public class Reversed_Array {
+	public static void main(String[] args) {
+
+		int[] arr = { 32, 35, 76, 54, 23, 45 };
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+ 			System.out.println(arr[i]);
+		}
+	}
+
+}
