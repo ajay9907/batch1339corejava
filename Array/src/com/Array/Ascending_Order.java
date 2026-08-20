eae2d3ccb5b047d6b0c201b4a0b4ab7e1e5e698e
Ajay Agwan <agwanajay28@gmail.com>
2026-08-20 14:42:45 -0400

    .

---
+++ Array/src/com/Array/Ascending_Order.java
@@ -0,0 +1,28 @@
+package com.Array;
+
+public class Ascending_Order {
+	public static void main(String[] args) {
+
+		int[] num = { 23, 223, 755, 78, 654, 90 };
+
+		for (int i = 0; i < num.length; i++) {
+			for (int j = i + 1; j < num.length; j++) {
+				if (num[i] >= num[j]) {
+					int temp = num[i];
+					num[i] = num[j];
+					num[j] = temp;
+ 
+				}
+
+			}
+
+		}
+		System.out.println("Ascending Order Array :");
+		for (int i = 0; i < num.length; i++) {
+
+			System.out.println(num[i]);
+		}
+
+	}
+
+}
