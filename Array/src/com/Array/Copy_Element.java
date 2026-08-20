f73322524989ab898e8c52bbc34594032ce43a50
Ajay Agwan <agwanajay28@gmail.com>
2026-08-20 13:23:21 -0400

    .

---
+++ Array/src/com/Array/Copy_Element.java
@@ -0,0 +1,20 @@
+package com.Array;
+
+public class Copy_Element {
+	public static void main(String[] args) {
+
+		int[] arr1 = { 22, 21, 45, 32, 54 };
+
+		int[] arr2 = new int[arr1.length];
+
+		for (int i = 0; i < arr1.length; i++) {
+
+			arr2[i] = arr1[i];
+		}
+		System.out.println("All Elements Copy in One Array :");
+		for (int i = 0; i < arr2.length; i++) {
+			System.out.println(arr2[i]);
+		}
+
+	}
+}
