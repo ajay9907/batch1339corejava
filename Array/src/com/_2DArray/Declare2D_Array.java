f2747eb0a15d8888aa1ed757706cf75f09fc3d86
Ajay Agwan <agwanajay28@gmail.com>
2026-08-22 13:29:05 -0400

    /

--- Array/src/com/_2DArray/Declare2D_Array.java
+++ Array/src/com/_2DArray/Declare2D_Array.java
@@ -6,14 +6,21 @@ public class Declare2D_Array {
 		int arr[][];
 		arr = new int[3][3];
 
-		int brr[][] = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80, 90 } };
-		for (int i = 0; i < brr.length; i++) {
-			for (int j = 0; j < brr.length; j++) {
-				System.out.println(brr[i][j]);
-
+		int brr[][] = { { 10, 20, 30 }, { 40, 50, 60, 50, 43, 55 }, { 70 } };
+//		for (int i = 0; i < brr.length; i++) {
+//			for (int j = 0; j < brr.length; j++) {
+//				System.out.println(brr[i][j]);
+//
+//			}
+//		}
+		
+		//
+		for (int rowIndex = 0; rowIndex <= brr.length - 1; rowIndex++) {
+			for (int colIndex = 0; colIndex <= brr[rowIndex].length - 1; colIndex++) {
+				System.out.print(brr[rowIndex][colIndex] + " ");
 			}
+			System.out.println();
 		}
-
 	}
 }
 
