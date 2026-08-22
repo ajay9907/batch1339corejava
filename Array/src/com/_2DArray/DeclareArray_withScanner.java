d10de57042c4a11c88201a9bbfc8b0c9e4c52677
Ajay Agwan <agwanajay28@gmail.com>
2026-08-22 14:12:39 -0400

    .

--- Array/src/com/_2DArray/DeclareArray_withScanner.java
+++ Array/src/com/_2DArray/DeclareArray_withScanner.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class DeclareArray_withScanner {
 	public static void main(String[] args) {
 
-		int[][] arr = new int[3][3];
+		int[][] arr = new int[3][4];
 
 		Scanner sc = new Scanner(System.in);
 
 		// i have used for take imput from user
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
+			for (int j = 0; j < arr[i].length; j++) {
 				System.err.println("Provides Elements for rows=" + i + "and col=" + j);
 
 				arr[i][j] = sc.nextInt();
@@ -21,7 +21,7 @@ public class DeclareArray_withScanner {
 		// i have used for printing each row and column elements
 
 		for (int row = 0; row < arr.length; row++) {
-			for (int col = 0; col < arr.length; col++) {
+			for (int col = 0; col < arr[row].length; col++) {
 				System.out.print(arr[row][col] + " ");
 			}
 			System.out.println();
