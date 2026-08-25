ed7f8b455eb5d071f3bcea27dfeef510af6e6860
Ajay Agwan <agwanajay28@gmail.com>
2026-08-25 14:36:44 -0400

    reverselist

---
+++ Collection_Framework/src/List/Reverse_Elements.java
@@ -0,0 +1,21 @@
+package List;
+//Write a Java program to reverse elements of a List.
+
+import java.util.ArrayList;
+
+public class Reverse_Elements {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> a = new ArrayList<>();
+		a.add(56);
+		a.add(33);
+		a.add(22);
+		a.add(78);
+		a.add(98);
+
+		System.out.println("Reverse List :" + a.reversed());
+		
+	}
+
+}
