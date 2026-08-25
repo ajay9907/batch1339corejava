3dce6b95c43aa04fe91e4ad915d55602e641d7f3
Ajay Agwan <agwanajay28@gmail.com>
2026-08-25 12:45:00 -0400

    string programs

---
+++ String/src/com/String_Practice/Palindrome.java
@@ -0,0 +1,29 @@
+package com.String_Practice;
+
+import java.util.Scanner;
+
+//Write a Java program to check whether a String is a palindrome or not, without using any inbuilt reverse method.
+
+public class Palindrome {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String: ");
+
+		String str = sc.nextLine();
+
+		String rev = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			char ch = str.charAt(i);
+			rev = rev + ch;
+		}
+		if (str.equals(rev)) {
+			System.out.println("Is Palindrome ");
+
+		} else {
+			System.out.println("Is Not Palindrome ");
+
+		}
+	}
+
+}
--- String/src/com/String_Practice/Reverse_String.java
+++ String/src/com/String_Practice/Reverse_String.java
@@ -1,14 +1,19 @@
 package com.String_Practice;
 
+import java.util.Scanner;
+
 //Write a Java program to reverse a String without using reverse() or any other reverse inbuilt method.
 
 public class Reverse_String {
 
-	static String getReverse(String str) {
+	static String getReverse() {
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter You're String : ");
+		String str = sc.nextLine();
 		String rev = "";
 		int n = str.length();
-		for (int i = n - 1; i > 0; i--) {
+		for (int i = n - 1; i >= 0; i--) {
 			char ch = str.charAt(i);
 			rev = rev + ch;
 		}
@@ -16,7 +21,6 @@ public class Reverse_String {
 	}
 
 	public static void main(String[] args) {
-		String str = "Ajay Agwan";
-		System.out.println(getReverse(str));
+		System.out.println(getReverse());
 	}
 }
