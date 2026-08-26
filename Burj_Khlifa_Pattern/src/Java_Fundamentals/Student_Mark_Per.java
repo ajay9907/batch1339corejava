cf57c601800242eef1f8da14b05ddcf601537404
Ajay Agwan <agwanajay28@gmail.com>
2026-08-26 07:15:26 -0400

    .

--- Burj_Khlifa_Pattern/src/Java_Fundamentals/Student_Mark_Per.java
+++ Burj_Khlifa_Pattern/src/Java_Fundamentals/Student_Mark_Per.java
@@ -1,29 +1,30 @@
 package Java_Fundamentals;
 
+import java.util.Scanner;
+
 //Write a Java program to calculate a student's total marks and percentage.
 
 public class Student_Mark_Per {
 	public static void main(String[] args) {
 
-		int math = 78;
-		int science = 85;
-		int english = 72;
+		Scanner sc = new Scanner(System.in);
 
-		int marks = 90;
+		System.out.println("Enter Your Subject ");
+		String sub = sc.nextLine();
+		System.out.println("Enter Math Marks :");
+		int math = sc.nextInt();
+		System.out.println("Enter Science Marks :");
+		int science = sc.nextInt();
+		System.out.println("Enter English Marks:");
+		int english = sc.nextInt();
+		System.out.println("Enter your Subject Marks:");
 
-		if (marks >= 90 && marks >= 80) {
-			System.out.println("Grade A : " + marks);
-		} else if (marks >= 70 && marks >= 60) {
-			System.out.println("Grade B :" + marks);
-		} else if (marks >= 45 && marks >= 35) {
-			System.out.println("Grade c :" + marks);
-		} else {
-			System.out.println("Fail...!");
+		System.out.println("Your Subject :" + sub);
 
-		}
 		int total = math + science + english;
+
 		System.out.println("Total Marks: " + total);
-		double percent = total * 3 / 100;
+		double percent = total / 300.00 * 100;
 		System.out.println("Percentage :" + percent);
 	}
 
